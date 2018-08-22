792a1072ae23e36638aad11d342e0ab091b9e204
Alisson <alissonchoffmann@gmail.com>
2018-08-21 22:16:11 -0300

    implementando_dbcontext

---
+++ Aulas/Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace Aula2108_CodeFirst.Modelos.DAL
+{
+    //tem q erdar da dbcontext (vem do entity)
+    public class Contexto : DbContext
+    {
+        //construtor para passar para o construtor do dbcontext indicar string de conexao
+
+        public Contexto() : base("nomeStringConexao")
+        {
+
+        }
+
+        DbSet<Categoria> Categorias { get; set; }
+        DbSet<Produto> Produtos { get; set; }
+
+    }
+}
