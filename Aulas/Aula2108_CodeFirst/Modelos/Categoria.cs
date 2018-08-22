5a7ba42b0c748a01387b130bc39088f2959768af
Alisson <alissonchoffmann@gmail.com>
2018-08-21 21:52:21 -0300

    categoria_e_produto

--- Aulas/Aula2108_CodeFirst/Modelos/Categoria.cs
+++ Aulas/Aula2108_CodeFirst/Modelos/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace Aula2108_CodeFirst.Modelos
         public int CategoriaID { get; set; }
 
         [Required]
+        [StringLength(30)]
         public string Nome { get; set; }
         public string Descricao { get; set; }
 
